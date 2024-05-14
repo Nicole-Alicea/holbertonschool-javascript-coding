2576603085a0dd96e1d3a5c1ba10a2f6cb4045bf
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-05-13 23:09:21 -0400

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const { error } = require('console');
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+});
