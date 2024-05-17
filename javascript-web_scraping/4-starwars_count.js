699ab4f53524b7a13ed4184ffad079090f653d46
Nicole <na.aliceanicole@gmail.com>
2024-05-16 22:58:41 -0400

    task 4 fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,9 +9,10 @@ request.get(process.argv[2], function (error, response, body) {
 
   const data = JSON.parse(body);
   let count = 0;
+  const targetURL = 'https://swapi-api.hbtn.io/api/people/18/';
 
   for (let x = 0; x < data.results.length; x++) {
-    if (data.results[x].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+    if (data.results[x].characters.some(char => char.includes(targetURL))) {
       count++;
     }
   }
