6072c07d3405f95c2f669192bc215da4c7e4a32f
Nicole <na.aliceanicole@gmail.com>
2024-05-16 22:26:24 -0400

    fixing semistandard validation

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    console.log('code:', response && response.statusCode);
+  if (error) {
+    console.log(error);
+  }
+  console.log('code:', response && response.statusCode);
 });
