4e108b27cec82c5afcb4d54bbfca490dd086db02
Nicole <na.aliceanicole@gmail.com>
2024-05-16 22:24:13 -0400

    fixing semistandard validation

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => {
-    if (error) {
-        return console.log(error);
-    }
+  if (error) {
+    return console.log(error);
+  }
 });
